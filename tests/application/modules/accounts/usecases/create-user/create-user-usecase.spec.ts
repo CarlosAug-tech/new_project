418523d4770f192bd133936be59fecf31895d209
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-05-31 10:51:35 -0300

    test: ensure CreateUserUseCase returns throw new Error if the Name field is not provided

--- tests/application/modules/accounts/usecases/create-user/create-user-usecase.spec.ts
+++ tests/application/modules/accounts/usecases/create-user/create-user-usecase.spec.ts
@@ -1,6 +1,19 @@
 import { CreateUserUseCase } from '@application/modules/accounts/usecases/create-user/create-user-usecase';
 
 describe('Create User UseCase', () => {
+  it('should not be able to create a new User if Name is not provided', async () => {
+    const sut = new CreateUserUseCase();
+
+    const user = {
+      name: '',
+      email: 'any_valid_email@mail.com',
+      password: 'any_valid_password',
+      confirmPassword: 'any_valid_password',
+    };
+
+    await expect(sut.execute(user)).rejects.toThrow();
+  });
+
   it('should be able to create a new User', async () => {
     const sut = new CreateUserUseCase();
 
