a0425f043706d5da398ee2d4495661bb54f6a029
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-05-31 10:58:30 -0300

    test: ensure CreateUserUseCase returns throw new Error if the ConfirmPassword field is not provided

--- tests/application/modules/accounts/usecases/create-user/create-user-usecase.spec.ts
+++ tests/application/modules/accounts/usecases/create-user/create-user-usecase.spec.ts
@@ -40,6 +40,19 @@ describe('Create User UseCase', () => {
     await expect(sut.execute(user)).rejects.toThrow();
   });
 
+  it('should not be able to create a new User if ConfirmPassword is not provided', async () => {
+    const sut = new CreateUserUseCase();
+
+    const user = {
+      name: 'any_name',
+      email: 'any_valid_email@mail.com',
+      password: 'any_valid_password',
+      confirmPassword: '',
+    };
+
+    await expect(sut.execute(user)).rejects.toThrow();
+  });
+
   it('should be able to create a new User', async () => {
     const sut = new CreateUserUseCase();
 
