a7c9826ab72bc4429dc4b17c7bcdb4a867d8a07f
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-05-31 10:57:33 -0300

    test: ensure CreateUserUseCase returns throw new Error if the Password field is not provided

--- tests/application/modules/accounts/usecases/create-user/create-user-usecase.spec.ts
+++ tests/application/modules/accounts/usecases/create-user/create-user-usecase.spec.ts
@@ -27,6 +27,19 @@ describe('Create User UseCase', () => {
     await expect(sut.execute(user)).rejects.toThrow();
   });
 
+  it('should not be able to create a new User if Password is not provided', async () => {
+    const sut = new CreateUserUseCase();
+
+    const user = {
+      name: 'any_name',
+      email: 'any_valid_email@mail.com',
+      password: '',
+      confirmPassword: 'any_valid_password',
+    };
+
+    await expect(sut.execute(user)).rejects.toThrow();
+  });
+
   it('should be able to create a new User', async () => {
     const sut = new CreateUserUseCase();
 
