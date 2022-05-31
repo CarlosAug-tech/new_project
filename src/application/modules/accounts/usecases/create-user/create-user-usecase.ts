834d9b2fa0079470ec94d6afc32afb1bcaec5d86
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-05-31 10:51:03 -0300

    feat: ensure CreateUserUseCase returns throw new Error if the Name field is not provided

--- src/application/modules/accounts/usecases/create-user/create-user-usecase.ts
+++ src/application/modules/accounts/usecases/create-user/create-user-usecase.ts
@@ -4,12 +4,16 @@ import {
 } from '../../dtos/CreateUserDTO';
 
 class CreateUserUseCase {
-  async execute({
-    name,
-    email,
-    password,
-    confirmPassword,
-  }: ICreateUserRequestDTO): Promise<ICreateUserResponseDTO> {
+  async execute(data: ICreateUserRequestDTO): Promise<ICreateUserResponseDTO> {
+    const { name, email, password, confirmPassword } = data;
+    const requiredFields = ['name', 'email', 'password', 'confirmPassword'];
+
+    for (const field of requiredFields) {
+      if (!data[field]) {
+        throw new Error(`This field ${field} is required!`);
+      }
+    }
+
     return {
       id: 'any_id',
       name,
