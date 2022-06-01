4e2ee434940755010ca9bf23986181538976c9c5
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-06-01 15:03:35 -0300

    feat: add hash method in BcryptProvider and CreateUserUseCase

--- src/infra/container/providers/EncryptProvider/implementations/bcrypt-provider.ts
+++ src/infra/container/providers/EncryptProvider/implementations/bcrypt-provider.ts
@@ -1,7 +1,13 @@
-import { compare } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 import { IEncryptProvider } from '../contracts/encrypt-provider';
 
 class BcryptProvider implements IEncryptProvider {
+  async hash(password: string, saltHash: number): Promise<string> {
+    const passwordHash = await hash(password, saltHash);
+
+    return passwordHash;
+  }
+
   async compare(password: string, password_hash: string): Promise<boolean> {
     const isMatch = await compare(password, password_hash);
 
