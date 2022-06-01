79bd115f9fe3c2736deb8ab5a1fcf5a9f8d8eb6b
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-06-01 12:56:57 -0300

    feat: ensure AuthenticationUserUseCase returns throw new Error if Email is not registered

--- src/application/modules/accounts/usecases/authentication-user/authentication-user-usecase.ts
+++ src/application/modules/accounts/usecases/authentication-user/authentication-user-usecase.ts
@@ -3,14 +3,27 @@ import {
   IAuthenticationUserRequestDTO,
   IAuthenticationUserResponseDTO,
 } from '../../dtos/authentication-user-dto';
+import { IUsersRepository } from '../../repositories/users-repository';
 
 class AuthenticationUserUseCase extends UseCase<
   IAuthenticationUserRequestDTO,
   IAuthenticationUserResponseDTO
 > {
+  constructor(private usersRepository: IUsersRepository) {
+    super();
+  }
+
   async perform(
     data: IAuthenticationUserRequestDTO,
   ): Promise<IAuthenticationUserResponseDTO> {
+    const { email, password } = data;
+
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('User or password invalid!');
+    }
+
     return {
       user: {
         id: 'any_id',
