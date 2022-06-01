bd1216200cc6c871d1f1e73a9bf1a4cd6812b81a
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-06-01 12:43:08 -0300

    refactor: move sut for a factory method

--- tests/application/modules/accounts/usecases/authentication-user/authentication-user-usecase.spec.ts
+++ tests/application/modules/accounts/usecases/authentication-user/authentication-user-usecase.spec.ts
@@ -1,13 +1,25 @@
 import { AuthenticationUserUseCase } from '@application/modules/accounts/usecases/authentication-user/authentication-user-usecase';
 
+interface ISutTypes {
+  sut: AuthenticationUserUseCase;
+}
+
+const makeSut = (): ISutTypes => {
+  const sut = new AuthenticationUserUseCase();
+
+  return {
+    sut,
+  };
+};
+
 describe('Authentication User UseCase', () => {
   it('should be able to authenticate a User', async () => {
+    const { sut } = makeSut();
+
     const credentials = {
       email: 'any_valid_email@mail.com',
       password: '1234',
     };
-
-    const sut = new AuthenticationUserUseCase();
 
     const response = await sut.execute(credentials);
 
